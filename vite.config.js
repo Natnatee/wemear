4009ec357752bc1ca90e6befffb36c156142d4fd
DiamondOMG <omgdigital.developer.02@gmail.com>
2025-10-04 22:38:56 +0700

    edit vite.config add       port: 5173,       host: "0.0.0.0",       hmr: {         host: "wemear.com", // **สำคัญ: แก้ไข HMR ให้ทำงานกับโดเมน (HTTPS)**         protocol: "wss", // **สำคัญ: ใช้ WebSocket Secure**       },

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-},{
-  server:{
-   https: true,
-   host:true 
+export default defineConfig(
+  {
+    plugins: [react()],
+  },
+  {
+    server: {
+      port: 5173,
+      host: "0.0.0.0",
+      hmr: {
+        host: "wemear.com", // **สำคัญ: แก้ไข HMR ให้ทำงานกับโดเมน (HTTPS)**
+        protocol: "wss", // **สำคัญ: ใช้ WebSocket Secure**
+      },
+    },
   }
-})
+);
