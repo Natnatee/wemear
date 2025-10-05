3d8210561a9fe9206efaf515b739951ec5ef7812
DiamondOMG <omgdigital.developer.02@gmail.com>
2025-10-06 00:09:07 +0700

    v

--- src/make_data/sample_project.js
+++ src/make_data/sample_project.js
@@ -1,5 +1,5 @@
 export const sampleProjects = [
-  { id:'1',user_id:'1',image: '/card_project/example_1.jpg',name: 'MoralMan', label: 'Designer', owner: 'Alice', date: '15 May 25 ',status:' Published',tool:'Image Tracking'},
+  { id:'1',user_id:'1',image: '/card_project/example_1.jpg',name: 'MoralMan', label: 'Designer', owner: 'Alice', date: '15 May 25 ',status:'Published',tool:'Image Tracking'},
   { id:'2',user_id:'1',image: '/card_project/example_2.jpg',name: 'Untitled project',label: 'Designer legacy', owner: 'Alice', date: '3 Mar 25 ',status:'Published',tool:'Image Tracking' },
   { id:'3',user_id:'1',image: '/card_project/example_3.jpg',name: 'Untitled project',label: 'Designer legacy', owner: 'John', date: '24 Dec 24', status:'Published',tool:'Image Tracking' },
   { id:'4',user_id:'1',image: '/card_project/example_4.jpg',name: 'Untitled project',label: 'Designer legacy', owner: 'Tony', date: '2 Nov 24', status:'Unpublished',tool:'Image Tracking' },
