1843076eac325d484a6c7abf6de3af363ab6198c
DiamondOMG <omgdigital.developer.02@gmail.com>
2025-10-05 22:16:22 +0700

    makedata + change floder name of card to card_project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,7 @@
 import React from "react";
 import Project from "@/components/Project";
+import { sampleProjects } from "@/make_data/sample_project";
 
-const sampleProjects = [
-  { id:'1',user_id:'1',image: '/card/example_1.jpg',name: 'MoralMan', label: 'Designer', owner: 'Alice', date: '15 May 25 ',status:' Published',tool:'Image Tracking'},
-  { id:'2',user_id:'1',image: '/card/example_2.jpg',name: 'Untitled project',label: 'Designer legacy', owner: 'Alice', date: '3 Mar 25 ',status:'Published',tool:'Image Tracking' },
-  { id:'3',user_id:'1',image: '/card/example_3.jpg',name: 'Untitled project',label: 'Designer legacy', owner: 'John', date: '24 Dec 24', status:'Published',tool:'Image Tracking' },
-  { id:'4',user_id:'1',image: '/card/example_4.jpg',name: 'Untitled project',label: 'Designer legacy', owner: 'Tony', date: '2 Nov 24', status:'Unpublished',tool:'Image Tracking' },
-  { id:'5',user_id:'1',image: '/card/example_5.jpg',name: 'TCDC Virtual 360',label: 'Designer', owner: 'Sarah', date: '12 Oct 24', status:'Published',tool:'Image Tracking' },
-  { id:'6',user_id:'1',image: '/card/example_6.jpg',name: 'TEST SOUND',label: 'Designer legacy', owner: 'David', date: '20 Sep 24', status:'Unpublished',tool:'Image Tracking' },
-  { id:'7',user_id:'1',image: '/card/example_7.jpg',name: 'Untitled project',label: 'Designer legacy', owner: 'Emily', date: '18 Sep 24', status:'Published',tool:'Face Tracking' },
-];
 
 function Home() {
   return <Project projects={sampleProjects} />
