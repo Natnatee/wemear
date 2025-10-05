29292a12565d6a486c236c249660832efef3d8ee
DiamondOMG <omgdigital.developer.02@gmail.com>
2025-10-05 22:35:34 +0700

    icon browser

--- src/components/CardProject.jsx
+++ src/components/CardProject.jsx
@@ -6,35 +6,44 @@ const CardProject = ({ project }) => {
       <img
         src={project.image}
         alt={project.name}
-        className="w-full h-48 object-cover "
+        className="w-full h-48 object-cover"
+      />
+      <img
+        src="/wemear.png"
+        alt="wemear"
+        className="absolute top-42 left-6 transform -translate-x-1/2 -translate-y-1/2 w-10 h-10 object-cover"
       />
       {project.label && (
         <div className="absolute top-2 left-2 bg-white text-blue-500 text-sm font-semibold px-2 py-1 rounded border border-gray-200">
           {project.label}
         </div>
       )}
-      <div className="p-4">
-        <div className="flex items-center space-x-2">
-          <svg
-            className="w-4 h-4" // ขนาด svg เล็กลง
-            viewBox="0 0 24 24"
-          >
-            <circle
-              cx="14" // อยู่ตรงกลาง svg
-              cy="14"
-              r="7" // ปรับ radius ให้พอดีกับ svg
-              fill={project.status === "Published" ? "#39FF14" : "gray"} // neon green
-              className={
-                project.status === "Published"
-                  ? "filter drop-shadow-[0_0_4px_#39FF14]"
-                  : ""
-              }
-            />
-          </svg>
-          <span className="text-sm text-gray-700">{project.name}</span>
+      <div className="py-8 px-6">
+        <div className="flex justify-between">
+          <div className="flex items-center space-x-2">
+            <svg className="w-4 h-4" viewBox="0 0 24 24">
+              <circle
+                cx="12"
+                cy="12"
+                r="7"
+                fill={project.status === "Published" ? "#39FF14" : "gray"}
+                className={
+                  project.status === "Published"
+                    ? "filter drop-shadow-[0_0_4px_#39FF14]"
+                    : ""
+                }
+              />
+            </svg>
+            <span className="text-sm text-gray-700">{project.name}</span>
+          </div>
+          <div>
+            {project.owner}
+          </div>
         </div>
 
-        <p className="text-sm text-gray-500 mt-1">Created {project.date} | {project.status}</p>
+        <p className="text-sm text-gray-500 mt-1">
+          Created {project.date} | {project.status}
+        </p>
       </div>
     </div>
   );
