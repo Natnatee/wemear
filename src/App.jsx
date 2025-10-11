6f19b2551f745be9a32df7f4bc5f4dda6a057686
DiamondOMG <omgdigital.developer.02@gmail.com>
2025-10-11 16:31:50 +0700

    Test_Ar_Mind

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import TestPage from "./pages/TestPage"; // 1. Import TestPage
 import NotFound from "./pages/NotFound";
 import TestComponent from "./pages/TestComponent";
 import NavbarWithSidebar from "./components/NavbarWithSidebar";
+import Test_Ar_Mind from "./pages/TestArMind"; // 2. Import Test_Ar_Mind
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
           <Route path="/testpage" element={<TestPage />} />
           <Route path="/testcomponent" element={<TestComponent />} />
           <Route path="*" element={<NotFound />} />
+          <Route path="/test_ar_mind" element={<Test_Ar_Mind />} />
         </Routes>
       </main>
     </Router>
---
+++ src/pages/TestArMind.jsx
@@ -0,0 +1,11 @@
+import TestArMind from "../components/tests/TestArMind";
+
+function Test_Ar_Mind() {
+  return (
+    <div >
+      <TestArMind />
+    </div>
+  );
+}
+
+export default Test_Ar_Mind;
